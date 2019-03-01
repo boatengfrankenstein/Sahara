852281f6eaddcd48b636d861e7bf85773bcc83a4
daniel <boatengfrankenstein@gmail.com>
2019-03-01 14:12:21 -0500

    CORS

--- Gemfile
+++ Gemfile
@@ -56,7 +56,7 @@ gem 'avatars_for_rails', '~> 0.2.4'
 gem 'dropzonejs-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+gem 'rack-cors'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,15 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+# require 'sprockets/railtie'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,12 +19,23 @@ module Autocomplete
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource \
+          '*',
+          headers: :any,
+          methods: %I[get post options delete patch puts]
+      end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
     
+
+
+ 
+
   end
 end
 
